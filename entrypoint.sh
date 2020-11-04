af7404e60200fad406a839756f5efa6234b9f5a8
Kodesnut <71318140+tastefinger@users.noreply.github.com>
2020-11-04 12:55:48 +0100

    prefix inputs with `INPUT_`

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,19 +2,19 @@
 
 # Setup ssh
 mkdir -p ~/.ssh
-echo "$SSH_PRIVATE_KEY" > ~/.ssh/id_rsa
+echo "$INPUT_SSH_PRIVATE_KEY" > ~/.ssh/id_rsa
 chmod 600 ~/.ssh/id_rsa
 
 # Rewrite authors
 git filter-branch --env-filter '
-	export GIT_COMMITTER_NAME="$ANNON_NAME"
-	export GIT_COMMITTER_EMAIL="$ANNON_EMAIL"
-	export GIT_AUTHOR_NAME="$ANNON_NAME"
-	export GIT_AUTHOR_EMAIL="$ANNON_EMAIL"
+	export GIT_COMMITTER_NAME="$INPUT_ANNON_NAME"
+	export GIT_COMMITTER_EMAIL="$INPUT_ANNON_EMAIL"
+	export GIT_AUTHOR_NAME="$INPUT_ANNON_NAME"
+	export GIT_AUTHOR_EMAIL="$INPUT_ANNON_EMAIL"
 	' --tag-name-filter cat -- --branches --tags
 
 # Update remote URL
-git remote set-url origin "$DESTINATION_GIT_URL"
+git remote set-url origin "$INPUT_DESTINATION_GIT_URL"
 
 # Push to destination
-git push --force --tags origin HEAD:"$MIRROR_BRANCH"
+git push --force --tags origin HEAD:"$INPUT_MIRROR_BRANCH"
